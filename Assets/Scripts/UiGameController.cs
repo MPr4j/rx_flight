b90be45fab6496d1444c63ebe653efc2311e64da
kourosh <kowroshy2219@gmail.com>
2023-08-26 12:06:55 +0330

    Koworsh UI Menu and Game Over implementation

---
+++ Assets/Scripts/UiGameController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiGameController : MonoBehaviour
+{
+    public GameOverScreen gameOverScreen;
+    int maxKill;
+    public void GameOver()
+    {
+        gameOverScreen.SetUp(maxKill);
+    }
+
+
+
+}
